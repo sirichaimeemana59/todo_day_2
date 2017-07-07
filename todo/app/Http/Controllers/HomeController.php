807a2ad3b8c9720cc3f1534bb39c55ba53c117fb
sirichaimeemana59 <sirichai_me59@live.rmutl.ac.th>
2017-07-07 14:48:48 +0700

    เรียน laravel

--- todo/app/Http/Controllers/HomeController.php
+++ todo/app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\categories;
+use App\Todo;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -13,21 +15,37 @@ class HomeController extends Controller
 
     public function create()
     {
-        return view('create');
+        $data = [
+            'categories' => categories::all()
+        ];
+        return view('create', $data);
+    }
+    public function store(Request $request){
+        //dd($request->all());
+        $todo = new Todo();
+        $todo->name = $request->input('name');
+        $todo->category_id = $request->input('category_id');
+        $todo->save();
+
+///////////////////////////////////////////////////////////////
+        //รับค่าที่ส่งมาจากฟอร์ม
+//        $todo = new Todo();
+//        $todo->name = $request->input('name');
+//        $todo->category_id = $request->input('category_id');
+//        $todo->save();
+
+
+        return redirect('/');
     }
 
-//    public function store(Request $request)
-//    {
-//        return redirect('/');
-//    }
-//
-//    public function edit($id){
-//         return view('edit');
-//    }
-//
-//        public function update(Request $request, $id)
-//        {
-//              return redirect('/');
-//
-//        }
+    public function edit($id){
+        //
+        return view('edit');
     }
+
+    public function update(Request $request, $id){
+        //
+        return redirect('/');
+    }
+
+}
--- todo/routes/web.php
+++ todo/routes/web.php
@@ -13,7 +13,7 @@
 
 Route::get('/', 'HomeController@index');
 Route::get('/create', 'HomeController@create');
-//Route::post('store', 'HomeController@store');
+Route::post('store', 'HomeController@store');
 //Route::get('edit/{id}', 'HomeController@edit');
 //Route::post('update/{id}', 'HomeController@update');
 //Route::get('delete/{id}', 'HomeController@delete');
